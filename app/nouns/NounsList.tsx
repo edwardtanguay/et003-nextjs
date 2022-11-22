eeb071893d38613b078d4aa61e18be23b7c3bcdd
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-22 08:52:16 +0100

    randomized

--- app/nouns/NounsList.tsx
+++ app/nouns/NounsList.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import axios from 'axios';
 import { Noun } from '../../typings';
 import Link from 'next/link';
+import * as tools from '../../tools';
 
 const url = 'https://edwardtanguay.vercel.app/share/germanNouns.json';
 
 async function NounsList() {
-	const nouns: Noun[] = (await axios.get(url)).data;
+	const rawNouns: Noun[] = (await axios.get(url)).data;
+	const nouns = tools.randomize(rawNouns);	
 	return (
 		<>
 			{nouns.map((noun: Noun) => {
